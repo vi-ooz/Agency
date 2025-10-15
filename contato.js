1d86198c44d2b73541cbdbe165781bc67b21d486
Vivian Queiroz <134158400+vi-ooz@users.noreply.github.com>
2025-10-15 15:09:49 -0400

    Add files via upload

--- contato.js
+++ contato.js
@@ -1,9 +1,9 @@
-const details = document.querySelectorAll('.details');
+const details = document.querySelectorAll('.detailsCont');
 
 details.forEach ((targetDetail) => {
 targetDetail.addEventListener('toggle', () => {
     if (targetDetail.open) {
-    details.forEach((other) => {
+    detailsCont.forEach((other) => {
         if (other !== targetDetail) other.open = false;
     });
     }
